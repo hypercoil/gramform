d0da884461b1203a1fc0243797de6d7e05d28739
Rastko Ciric <rastko@stanford.edu>
2023-08-17 01:30:54 -0700

    (MAINT) dev stack boilerplate

---
+++ noxfile.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+Noxfile
+"""
+import nox
+
+@nox.session()
+def clean(session):
+    session.install('coverage[toml]')
+    session.run('coverage', 'erase')
+
+@nox.session(python=["3.10", "3.11"])
+def tests(session):
+    session.install('jax[cpu]')
+    session.install('.[full,test]')
+    session.run(
+        'pytest',
+        '--cov', 'gramform',
+        '--cov-append',
+        'tests/',
+    )
+    session.run('ruff', 'check', 'src/gramform')
+    session.run('blue', '--check', 'src/gramform')
+
+@nox.session()
+def report(session):
+    session.install('coverage[toml]')
+    session.run(
+        'coverage',
+        'report', '--fail-under=90',
+        "--omit='*test*,*__init__*'",
+    )
+    session.run(
+        'coverage',
+        'html',
+        "--omit='*test*,*__init__*'",
+    )
+    session.run(
+        'coverage',
+        'xml',
+        "--omit='*test*,*__init__*'",
+    )
